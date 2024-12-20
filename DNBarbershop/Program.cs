db4433b2c08421564a677b4470fe432daa5a57b5
aykutyamak <aykut2006yamak@gmail.com>
2024-12-20 10:07:15 +0200

    Initial Setup

--- DNBarbershop/Program.cs
+++ DNBarbershop/Program.cs
@@ -1,9 +1,12 @@
+using DNBarbershop.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<>
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
