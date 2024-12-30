fc6bdd7c306f1b8d672a21333cb17f792e3ee33e
AykutYamak <aykut2006yamak@gmail.com>
2024-12-30 15:13:25 +0200

    Added Repository to Data-access layer

--- DNBarbershop.DataAccess/Repository/IRepository.cs
+++ DNBarbershop.DataAccess/Repository/IRepository.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DNBarbershop.DataAccess.Repository
 {
-    internal interface IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void DeleteAll();
+        void Find(Expression<Func<T, bool>> filter);
+
+
     }
 }
--- DNBarbershop.DataAccess/Repository/Repository.cs
+++ DNBarbershop.DataAccess/Repository/Repository.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DNBarbershop.DataAccess.Repository
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private ApplicationDbContext db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext _db)
+        {
+            db= _db;
+            dbSet = db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Find(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
